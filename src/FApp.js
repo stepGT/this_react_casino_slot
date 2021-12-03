1e70a80a2aa583ae40471c0d16ce49c6c5fb8442
stepGT <stepGT@mail.ru>
2021-12-03 21:36:36 +1000

    Refactor FApp

--- src/FApp.js
+++ src/FApp.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 
 import { RepeatButton } from './components';
 import { WinningSound } from './components';
@@ -6,28 +6,27 @@ import { Spinner } from './components';
 
 import './App.css';
 
+let repeatButton = null;
+let winningSound = null;
+let matches = [];
+let _child1, _child2, _child3;
+const loser = [
+  'Not quite',
+  'Stop gambling',
+  'Hey, you lost!',
+  'Ouch! I felt that',
+  "Don't beat yourself up",
+  'There goes the college fund',
+  'I have a cat. You have a loss',
+  "You're awesome at losing",
+  'Coding is hard',
+  "Don't hate the coder",
+];
+
 const FApp = () => {
   const [winner, setWinner] = useState(null);
-  const [matches, setMatches] = useState([]);
-  const ref = useCallback((component) => {
-    if (component) component.forceUpdateHandler();
-  }, []);
-  let repeatButton = null;
-  let winningSound = null;
-  const loser = [
-    'Not quite',
-    'Stop gambling',
-    'Hey, you lost!',
-    'Ouch! I felt that',
-    "Don't beat yourself up",
-    'There goes the college fund',
-    'I have a cat. You have a loss',
-    "You're awesome at losing",
-    'Coding is hard',
-    "Don't hate the coder",
-  ];
   const finishHandler = (value) => {
-    setMatches(value);
+    matches.push(value);
     //
     if (matches.length === 3) {
       const first = matches[0];
@@ -35,10 +34,16 @@ const FApp = () => {
       setWinner(results);
     }
   };
+
   const handleClick = () => {
     setWinner(null);
-    setMatches([]);
+    emptyMatches();
+    _child1.forceUpdateHandler();
+    _child2.forceUpdateHandler();
+    _child3.forceUpdateHandler();
   };
+
+  const emptyMatches = () => (matches = []);
 
   const getLoser = () => {
     return loser[Math.floor(Math.random() * loser.length)];
@@ -65,9 +70,27 @@ const FApp = () => {
       </h1>
 
       <div className={`spinner-container`}>
-        <Spinner onFinish={finishHandler} ref={ref} timer="1000" />
-        <Spinner onFinish={finishHandler} ref={ref} timer="1400" />
-        <Spinner onFinish={finishHandler} ref={ref} timer="2200" />
+        <Spinner
+          onFinish={finishHandler}
+          ref={(child) => {
+            _child1 = child;
+          }}
+          timer="1000"
+        />
+        <Spinner
+          onFinish={finishHandler}
+          ref={(child) => {
+            _child2 = child;
+          }}
+          timer="1400"
+        />
+        <Spinner
+          onFinish={finishHandler}
+          ref={(child) => {
+            _child3 = child;
+          }}
+          timer="2200"
+        />
         <div className="gradient-fade"></div>
       </div>
       {repeatButton}
